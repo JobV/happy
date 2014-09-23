333ae6ad541bd5957e513cede6f65cd78c731ad6
Job van der Voort <jobvandervoort@gmail.com>
2014-09-23 23:44:31 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'responses#index'
   resource :responses
   devise_for :users
-  get "/email_processor", to: proc { [200, {}, ["OK"]] }, as: "mandrill_head_test_request"
+  mount_griddler
 end
