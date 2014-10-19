b53396be989d95486a2c0474fd9140f2f2212195
Job van der Voort <jobvandervoort@gmail.com>
2014-10-19 21:47:01 +0200

    mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => '2me.io' }
+  config.action_mailer.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'smtp.mandrillapp.com',
+      :authentication => :plain
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
