caa17690b034f15f4e9db62a49ef2025b1f0e478
Job van der Voort <jobvandervoort@gmail.com>
2014-10-21 01:04:03 +0200

    rails 4.2

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source 'https://rubygems.org'
 
 ruby '2.1.2'
 
-gem 'rails', '4.1.6'
-gem 'sass-rails', '~> 4.0.3'
+gem 'rails', '4.2.0beta2'
+gem 'sass-rails', '5.0.0.beta1'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
--- app/controllers/mail_query_controller.rb
+++ app/controllers/mail_query_controller.rb
@@ -1,7 +1,7 @@
 class MailQueryController < ApplicationController
 
   def happiness
-    QuestionMailer.happiness(Person.first).deliver
+    QuestionMailer.happiness(Person.first).deliver_later
     respond_to do |f|
       f.js {}
       f.html { redirect_to root_path }
