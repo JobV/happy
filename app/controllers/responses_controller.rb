2c17f2f5f9fb14318d73bca04951d74c3aa62464
Job van der Voort <jobvandervoort@gmail.com>
2014-10-25 22:07:21 +0200

    small changes

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -63,6 +63,7 @@ class ResponsesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_response
+      @conversations_page = true;
       @response = Response.find(params[:id])
     end
 
