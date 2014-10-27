711e26b6503498135982ac88252c631cf56c364a
Job van der Voort <jobvandervoort@gmail.com>
2014-10-27 21:58:52 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :people
 
-  root to: 'responses#index'
+  root to: 'dashboard#index'
   resources :responses
   get '/admin', to: 'admin#index', as: :admin
   devise_for :users
   mount_griddler
 
+  get '/dashboard',     to: 'dashboard#index', as: :dashboard
+  get '/conversations', to: 'responses#index', as: :conversations
   post '/query_happiness', to: 'mail_query#happiness', as: :query_happiness
 end
