dd49a021a8336ba9bbb7446538a63cd19d65b0aa
Job van der Voort <jobvandervoort@gmail.com>
2014-10-27 21:40:04 +0100

    mail everyone

--- app/controllers/mail_query_controller.rb
+++ app/controllers/mail_query_controller.rb
@@ -1,7 +1,13 @@
 class MailQueryController < ApplicationController
 
   def happiness
-    QuestionMailer.happiness(Person.first).deliver_later
+    Person.all.each do |p|
+      begin
+        QuestionMailer.happiness(p).deliver_later
+      rescue Net::SMTPAuthenticationError, Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e
+        flash[:error] << "Something went wrong with #{p.first_name}"
+      end
+    end
     respond_to do |f|
       f.js {}
       f.html { redirect_to root_path }
