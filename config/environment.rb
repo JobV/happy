eca7c8a60b74a4b087f50028b3b1078a9872e157
Job van der Voort <jobvandervoort@gmail.com>
2014-11-02 15:20:16 +0100

    logglier i/o logentries

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-if Rails.env.development?
-  Rails.logger = Le.new('c90b5a6a-1843-4961-918c-45a14ae6f9b5', debug: true)
-else
-  Rails.logger = Le.new('c90b5a6a-1843-4961-918c-45a14ae6f9b5')
-end
